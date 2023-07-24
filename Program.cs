9b5dacc1b0cdacc5d6a7a7d5a69168f1499309b4
R22DOV11022000 <R22DOV11022000@63-068-012-WS5>
2023-07-24 04:51:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace testprog
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddScoped(sp =>
+            new HttpClient
+            {
+                BaseAddress = new Uri("http://sed.rr22.local:8080/")
+            });
+            builder.Services.AddLogging();
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBrowserExtensionServices();
+            await builder.Build().RunAsync();
+        }
+    }
+}
